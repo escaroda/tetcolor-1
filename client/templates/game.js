34414f2e9116d58607020e94c17a76e601303ec8
Nikita Shatunov <nikita.shatunov@gmail.com>
2015-09-10 11:32:03 +0300

    basic movement

--- client/templates/game.js
+++ client/templates/game.js
@@ -3,38 +3,60 @@ var firstRender = true;
 var listRenderHold = LaunchScreen.hold();
 listFadeInHold = null;
 
-Template.game.onRendered(function() {
-  if (firstRender) {
-    // Released in app-body.js
-    listFadeInHold = LaunchScreen.hold();
 
-    // Handle for launch screen defined in app-body.js
-    listRenderHold.release();
+Template.game.onRendered(function () {
+    if (firstRender) {
+        // Released in app-body.js
+        listFadeInHold = LaunchScreen.hold();
 
-    firstRender = false;
-  }
+        // Handle for launch screen defined in app-body.js
+        listRenderHold.release();
 
-    /*TODO
-  this.find('.js-title-nav')._uihooks = {
-    insertElement: function(node, next) {
-      $(node)
-        .hide()
-        .insertBefore(next)
-        .fadeIn();
-    },
-    removeElement: function(node) {
-      $(node).fadeOut(function() {
-        this.remove();
-      });
+        firstRender = false;
     }
-  };*/
+    var el = this.find('.element-matrix'),
+        blockSize = 30,
+
+        handleRight = function () {
+            el.style.transform = "translate(" + blockSize + "px)"
+        },
+        handleLeft = function () {
+            el.style.transform = "translate(-" + blockSize + "px)"
+        };
+
+    $('body').on('keydown', function (e) {
+        if (event.which == 13) {
+            event.preventDefault();
+        }
+        console.log('keydown', event.which);
+        switch (event.which) {
+        case 39:
+            console.log('right');
+            handleRight();
+            break;
+        case 37:
+            console.log('left');
+            handleLeft();
+            break;
+        case 38:
+            console.log('up');
+            break;
+        case 40:
+            console.log('down');
+            break;
+        case 32:
+            console.log('spacebar');
+        }
+    });
 });
 
 Template.game.helpers({
-  //heleprs
+    //heleprs
 });
 
 
 Template.game.events({
-
+    'body keypress': function (e) {
+        console.log('key', e);
+    }
 });
